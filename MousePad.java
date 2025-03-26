4a05fe40b7ffe0e123f8e22995b68eabfbe3b7bb
TheEmeraldPants <153216120+TheEmeraldPants@users.noreply.github.com>
2025-03-26 21:52:39 +0000

    fixed mousepad error

--- MousePad.java
+++ MousePad.java
@@ -2,6 +2,6 @@ public class MousePad extends Peripheral {
     protected String surfaceType;
 
     public MousePad(double p, String b, boolean rgb, int pCon, String desc, double w, double[] d, String i, String sType){
-        super(p, b, rgb, pCon, desc, w, d, i, s)
+        super(p, b, rgb, pCon, desc, w, d, i, s);
     }
 }
